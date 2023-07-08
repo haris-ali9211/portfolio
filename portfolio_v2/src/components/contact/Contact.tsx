54f0088106e35ae6c6e884a856bc6d81b50a0dad
Haris Ali <haris.aali9211@gmail.com>
2023-07-09 02:26:35 +0500

    update

--- portfolio_v2/src/components/contact/Contact.tsx
+++ portfolio_v2/src/components/contact/Contact.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { Button } from "@chakra-ui/react";
 import { MdOutlineEmail } from "react-icons/md";
-import { AiFillGithub, AiFillTwitterCircle, AiOutlineArrowRight } from "react-icons/ai";
+import {
+  AiFillGithub,
+  AiFillTwitterCircle,
+  AiOutlineArrowRight,
+} from "react-icons/ai";
 import { BsInstagram } from "react-icons/bs";
 import { IoLocationOutline } from "react-icons/io5";
 import { FaDiscord } from "react-icons/fa";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import "./Contact.css";
 
 export type Contact = {
-  name: string,
-  info: string,
-  icon: React.ReactElement,
-  url: string,
-}
+  name: string;
+  info: string;
+  icon: React.ReactElement;
+  url: string;
+};
 
 export const contacts: Contact[] = [
   {
@@ -49,38 +53,46 @@ export const contacts: Contact[] = [
 ];
 
 const Contact = () => {
-  return(
+  return (
     <div className="bg-contact">
       <div className="contact-header">
         <p className="heading">Contacte Me</p>
-        <p className="text">If you have any questions or want to hire me, please contact me:</p>
+        <p className="text">
+          If you have any questions or want to hire me, please contact me:
+        </p>
       </div>
 
       <div className="contact-grid">
         {contacts.map((contact: Contact) => {
-          return(
+          return (
             <a key={uuidv4()} href={contact.url} target="_blank">
               <div className="contact-section">
-                  {contact.icon}
-                  <div className="contact-text-div">
-                    <p className="contact-title">{contact.name}</p>
-                    <p className="contact-details">{contact.info}</p>
-                  </div>
+                {contact.icon}
+                <div className="contact-text-div">
+                  <p className="contact-title">{contact.name}</p>
+                  <p className="contact-details">{contact.info}</p>
+                </div>
               </div>
             </a>
-          )
+          );
         })}
       </div>
 
       <div className="button-container">
         <a href="mailto:hariss.ali9211@gmail.com">
-          <Button size='lg' rightIcon={<AiOutlineArrowRight />} colorScheme="blue" variant='solid' className="contact-btn">
+          <Button
+            size="lg"
+            rightIcon={<AiOutlineArrowRight />}
+            colorScheme="blue"
+            variant="solid"
+            className="contact-btn"
+          >
             Contact Me
           </Button>
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Contact;
